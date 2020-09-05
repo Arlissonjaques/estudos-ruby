7b6cbcc24ab0b7fa53d035229bfeab739a72ae1f
Arlisson Jaques <arlissonjaques@gmail.com>
2020-09-05 16:57:20 -0500

    Entrada de dados

---
+++ entrada_de_dados.rb
@@ -0,0 +1,10 @@
+
+# print é igual ao puts, porem nao quebra linha apos a execucao
+
+print "Digite seu nome: " 
+nome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.to_i
+
+puts nome, idade
