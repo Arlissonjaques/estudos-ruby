6b21bc73e371a4da0e6c2042b0d23b9d21510136
Arlisson Jaques <arlissonjaques@gmail.com>
2020-09-05 18:08:16 -0500

    Condicinal unless

---
+++ condicional_unless.rb
@@ -0,0 +1,10 @@
+# unless é o contrario do if, ele executa caso a codicao
+# seja falsa
+
+estado_da_loja = "aberto"
+
+unless estado_da_loja != "aberto"
+    puts "Você pode entrar!"
+else
+    puts "Você não pode entrar"
+end
